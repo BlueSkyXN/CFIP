50a127ef17ad8202495ef99c2d8ea4ac8fdaf908
BlueSkyXN <bluesky@000714.xyz>
2022-06-03 12:41:55 +0800

    3.1.8

--- CFIP/3.1/单IP测速.bat
+++ CFIP/3.1/单IP测速.bat
@@ -5,7 +5,7 @@ color A
 setlocal enabledelayedexpansion
 cls
 set /p a=请输入优选 IP :
-curl --ipv4 --resolve cfcf.mihoyo.tk:443:%a% https://cfcf.mihoyo.tk/data.txt -o temp.txt -#
+curl --ipv4 --resolve cfcf.mihoyo.tk:443:%a% http://cfcf.mihoyo.tk/data.txt -o temp.txt -#
 for /f "tokens=2 delims==" %%a in ('findstr /C:"domain" temp.txt') do (
 set domain=%%a
 )
@@ -14,5 +14,5 @@ set file=%%a
 )
 del temp.txt
 title  正在测试 %a%
-curl --resolve !domain!:443:%a% https://!domain!/!file! -o nul
+curl --resolve !domain!:443:%a% http://!domain!/!file! -o nul
 pause
